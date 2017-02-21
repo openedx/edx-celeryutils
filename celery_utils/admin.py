25d8a72a4cc2b0a45c5fa9037e9dbda22493a066
J. Cliff Dyer <cdyer@edx.org>
2017-02-21 16:21:35 -0500

    Define FailedTask admin

---
+++ celery_utils/admin.py
@@ -0,0 +1,21 @@
+"""
+Admin site configuration
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.contrib import admin
+
+from .models import FailedTask
+
+
+class FailedTaskAdmin(admin.ModelAdmin):
+    """
+    Customized admin for FailedTask model
+    """
+    date_hierarchy = 'created'
+    list_display = ['task_id', 'task_name', 'args', 'kwargs', 'created', 'datetime_resolved']
+    list_filter = ['task_name']
+    search_fields = ['task_name', 'task_id', 'args', 'kwargs']
+
+admin.site.register(FailedTask, FailedTaskAdmin)
