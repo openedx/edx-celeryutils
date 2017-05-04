ddf0b19b35238b4718096f8fd5636ff578acd2dc
Eric Fischer <efischer@edx.org>
2017-05-04 12:53:36 -0400

    Explicitly specify django-celery dependency

---
+++ celery_utils/migrations/0002_backend_cleanup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('celery_utils', '0001_initial'),
+        ('djcelery', '0001_initial'),
+    ]
+
+    operations = [
+    ]
