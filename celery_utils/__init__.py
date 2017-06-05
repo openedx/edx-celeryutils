7ba39dc4c28beba495f90a352c1c61f746527434
Eric Fischer <efischer@edx.org>
2017-06-05 11:11:15 -0400

    Bump internal version to 0.2.2

--- celery_utils/__init__.py
+++ celery_utils/__init__.py
@@ -5,6 +5,6 @@ Code to support working with celery.
 from __future__ import absolute_import, unicode_literals
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 default_app_config = 'celery_utils.apps.CeleryUtilsConfig'  # pylint: disable=invalid-name
