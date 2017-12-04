9e7826d4b771214aa2904aef6f8f9cdb722d3e08
Nimisha Asthagiri <nasthagiri@edx.org>
2017-12-03 23:43:13 -0500

    Add logging when task fails and is not retried

--- tests/test_logged_task.py
+++ tests/test_logged_task.py
@@ -13,12 +13,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from billiard.einfo import ExceptionInfo
 import mock
+import pytest
 
 from celery_utils.logged_task import LoggedTask
 from test_utils import tasks
 
 
-def test_logged_task_without_failure():
+def test_no_failure():
     with mock.patch('celery_utils.logged_task.log') as mocklog:
         result = tasks.simple_logged_task.apply_async(args=(3, 4), kwargs={'c': 5}, task_id='papers-please')
         result.wait()
@@ -26,9 +27,21 @@ def test_logged_task_without_failure():
     stringc = 'c'  # Handle different string repr for python 2 and 3
     logmessage = "Task test_utils.tasks.simple_logged_task[papers-please] submitted with arguments (3, 4), {%r: 5}"
     mocklog.info.assert_called_with(logmessage % stringc)
+    assert not mocklog.error.called
 
 
-def test_on_retry_formatting():
+def test_failure():
+    with mock.patch('celery_utils.logged_task.log') as mocklog:
+        result = tasks.failed_logged_task.delay()
+        with pytest.raises(ValueError):
+            result.wait(retry=False)
+    assert result.status == 'FAILURE'
+    assert mocklog.error.called
+    log_message = mocklog.error.call_args[0][0]
+    assert '[{}] failed due to Traceback'.format(result.task_id) in log_message
+
+
+def test_retry():
     # With celery running in eager mode, the on_retry handler doesn't actually
     # get called when a retry happens.  Here we just try to show that when it does
     # get called, the log message is formatted correctly.
@@ -45,15 +58,3 @@ def test_on_retry_formatting():
             logmessage = mocklog.warning.call_args[0][0]
             assert '[{}]'.format(task_id) in logmessage
             assert einfo.traceback in logmessage
-
-
-def test_no_capture_on_retry_unless_exception():
-    task = LoggedTask()
-    task_id = 'my-id'
-    args = (1, 2)
-    kwargs = {'c': 3}
-    exc = None
-    einfo = None
-    with mock.patch('celery_utils.logged_task.log') as mocklog:
-        task.on_retry(exc, task_id, args, kwargs, einfo)
-        assert not mocklog.warning.called
