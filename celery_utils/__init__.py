6a8d691d4364b3228ce0a31a99dff3e36f91d049
usamasadiq <usama.sadiq@arbisoft.com>
2020-06-30 16:18:34 +0500

    Releasing new version

--- celery_utils/__init__.py
+++ celery_utils/__init__.py
@@ -2,6 +2,6 @@
 Code to support working with celery.
 """
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 default_app_config = 'celery_utils.apps.CeleryUtilsConfig'  # pylint: disable=invalid-name
