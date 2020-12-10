a39faba4b9d20e4a0842d7f390b73726678d2fc4
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-12-10 23:13:01 +0500

    BOM-2168 Adding celery into tox for multiple versions testing.

--- celery_utils/__init__.py
+++ celery_utils/__init__.py
@@ -2,6 +2,6 @@
 Code to support working with celery.
 """
 
-__version__ = '0.5.4'
+__version__ = '0.5.3'
 
 default_app_config = 'celery_utils.apps.CeleryUtilsConfig'  # pylint: disable=invalid-name
