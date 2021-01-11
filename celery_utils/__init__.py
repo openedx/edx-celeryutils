98d746a18145c3e2679f5c1cbc479d83a1daa88e
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2021-01-11 18:23:57 +0500

    Remove upper constraint for Django

--- celery_utils/__init__.py
+++ celery_utils/__init__.py
@@ -2,6 +2,6 @@
 Code to support working with celery.
 """
 
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 
 default_app_config = 'celery_utils.apps.CeleryUtilsConfig'  # pylint: disable=invalid-name
