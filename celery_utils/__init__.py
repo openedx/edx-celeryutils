0b96453fcb05207800c5cec4f648aa37ce48cdd7
Jawayria <39649635+Jawayria@users.noreply.github.com>
2021-07-12 13:40:24 +0500

    feat: Added support for django3.2 (#76)

--- celery_utils/__init__.py
+++ celery_utils/__init__.py
@@ -2,6 +2,6 @@
 Code to support working with celery.
 """
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 default_app_config = 'celery_utils.apps.CeleryUtilsConfig'  # pylint: disable=invalid-name
