8f1d4c47590b711c7bc08e311eb81625c0102d1b
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-06-15 20:36:46 +0500

    fix: Support added for Django 4.2

--- celery_utils/__init__.py
+++ celery_utils/__init__.py
@@ -2,6 +2,6 @@
 Code to support working with celery.
 """
 
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 
 default_app_config = 'celery_utils.apps.CeleryUtilsConfig'  # pylint: disable=invalid-name
