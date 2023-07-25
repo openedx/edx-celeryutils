442917749dbcf269c79183f58e3e12b1081cde8c
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-07-25 20:34:34 +0500

    fix: url.py reverted

--- urls.py
+++ urls.py
@@ -2,9 +2,9 @@
 Simple URL Configuration for test application.
 """
 
-from django.urls import re_path
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-        re_path(r'^admin/', admin.site.urls),
+        url(r'^admin/', admin.site.urls),
 ]
